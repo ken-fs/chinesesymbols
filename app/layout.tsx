a3826dd837721cab335872a0a24cb66667cf7f8e
ljw6688 <493129720ljw@gmail.com>
2025-09-14 21:13:36 +0800

    feat: 配置优化

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import UserPreferencesProvider from "@/components/UserPreferencesProvider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chinese Symbols",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body suppressHydrationWarning={true}>
         <UserPreferencesProvider>{children}</UserPreferencesProvider>
       </body>
     </html>
